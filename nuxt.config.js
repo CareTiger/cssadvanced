7eaa1824d8f1781f3d386cb9bb6ff4d0ad71aeb1
venkateshvadlamani <venkatesh.caretiger@gmail.com>
2019-10-20 14:50:37 -0500

    updates

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', href: '/lotus.png' },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Fredoka+One|Poppins&display=swap" },      
+      { rel: 'stylesheet', href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" },    
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/icon?family=Material+Icons" },    
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~assets/main.scss'
+  ],
+
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
+}
