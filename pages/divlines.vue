6539c51faaeef01438f83d4d930b7963b4e57d82
venkateshvadlamani <venkatesh.caretiger@gmail.com>
2019-10-23 09:17:43 -0500

    updates

---
+++ pages/divlines.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="divlines">
+
+        <div ref="div1" id="div1" style="width: 100px; height: 100px; top:0; left:0; background:#e53935 ; position:absolute;"></div>
+        <div ref="div2" id="div2" style="width: 100px; height: 100px; top:0; left:300px; background:#4527a0 ; position:absolute;"></div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.divlines {
+    position: relative;
+}
+
+</style>
+
+<script>
+export default {
+    data() {
+        return{}
+    },
+    methods: {
+        myClick() {
+            console.log(this.$refs)
+        }
+    }
+    
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -27,11 +27,11 @@
     </article>
 
     <article>
-      <h3>Good designs layouts</h3>
+      <h3>Noteworthy designs layouts - ILLUSTRATIONS</h3>
 
       <section>
         <h4>
-          <a href="https://www.cssdesignawards.com/sites/discovery-land-company/35901/">Discovery land company</a>
+          <a href="https://discoverylandco.com/">Discovery land company</a>
         </h4>
         <p>Why I like? an elegant layout with nice tranistions. a pleasing UI with a nice UX.</p>
       </section>
@@ -40,34 +40,85 @@
         <h4>
           <a href="https://www.deplacemaison.com/">Deplace Mason</a>
         </h4>
-        <p>Why I like? Nice use of verticalility in text, using illustrations and real images to highlight products.</p>
+        <p>Nice use of verticalility in text, using illustrations and real images to highlight products.</p>
       </section>
       
       <section>
         <h4>
           <a href="https://findingctrl.nesta.org.uk/">Finding Control -Vision for the future internet</a>
         </h4>
-        <p>Why I like? Nice use of verticalility in text, using illustrations and real images to highlight products.</p>
+        <p>Nice use of verticalility in text, using illustrations and real images to highlight products.</p>
       </section>
 
       <section>
         <h4>
           <a href="https://zumi.gucci.com/">Gucci Zumi- Gucci crushes it again </a>
         </h4>
-        <p>Why I like? Nice use of verticalility in text, using illustrations and real images to highlight products.</p>
+        <p>Gucci is Gucci. Insanely awesome illustrations and transitions.</p>
       </section>
 
+      <section>
+        <h4>
+          <a href="https://www.designbetter.co/books/">Design Better </a>
+        </h4>
+        <p>Lots of amazing illustrations. More importantly there are lots of interesting mini-books for Desgin </p>
+      </section>
+
+      <section>
+        <h4>
+          <a href="https://www.boranka.hr/en?lq=1">Boranka is a Croatian environmental company</a>
+        </h4>
+        <p>I like this site because it pulls the user in for an interatice experience. </p>
+      </section>
+
+      <section>
+        <h4>
+          <a href="https://pureemaison.com/">Puree Maison is a digital agency based out of Paris.</a>
+        </h4>
+        <p>I like the use of grids, animation and overlay. The videos and image tranistions are attention grabbing.</p>
+      </section>
+
+      <section>
+        <h4>
+          <a href="https://eatgenesis.com/">"Eat Genesis" is a food/restuarant company out of London.</a>
+        </h4>
+        <p>Nice illustrations with the "Genesis" theme. Also nice use of Black/White colors.</p>
+      </section>
+
+
+
     </article>
+
+
+
+    <article>
+      <h3>Noteworthy designs layouts - TRAVEL</h3>
+
+      <section>
+        <h4>
+          <a href="https://festival.bersiserlini.it/en">"Ber Seserlini" is italian tours and travel company.</a>
+        </h4>
+        <p>Beautiful illustrations that bring out the themes associated with the events. nice layouts and use of cursors.</p>
+      </section>
+      
+      <section>
+        <h4></h4>
+        <p></p>
+      </section>
+
+
+    </article>
+
 
   </div>
 </template>
 
 <style lang="scss" scoped>
 .home {
-  padding: 3rem;
+  padding-left: 3rem;
 
   section {
-    padding: 1rem;
+    padding: 0.5rem;
   }
 }
 
