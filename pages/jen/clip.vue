aa9f23d312b91c239351e4615b4ba59552c53351
venkateshvadlamani <venkatesh.caretiger@gmail.com>
2019-10-25 14:12:30 -0500

    updates

--- pages/jen/clip.vue
+++ pages/jen/clip.vue
@@ -1,12 +1,31 @@
 <template>
-    <div>
-        <h1>Pixel the Kitten</h1>
+    <div class="pixel debug">
+        <h1 class="">Pixel the Kitten</h1>
         <h2>Like playing with grid</h2>                
         <figure>
             <img src="/img/jen/people/19635836264_927fbee8b5_b.jpg" alt="">
-            <img src="/img/jen/people/19635836264_927fbee8b5_b.jpg" alt="">
-            <img src="/img/jen/people/19635836264_927fbee8b5_b.jpg" alt="">
+            <hr>
+            <img src="/img/jen/people/20070461390_e350b158e3_b.jpg" alt="">
+            <hr>
+            <img src="/img/jen/people/20258466845_9d192564f0_b.jpg" alt="">
+            <hr>
+            <p class="">
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" 
+            </p>
         </figure>
 
     </div>
 </template>
+
+<style lang="scss" scoped>
+.pixel {
+    
+}
+
+img {
+    width: 30vw;
+    height: 60vh;
+    object-fit: cover;
+    object-position: 50% 10%;
+}
+</style>
