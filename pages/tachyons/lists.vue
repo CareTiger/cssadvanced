45588cf167fabac8d5736e8b12014323f77dc0f2
venkateshvadlamani <venkatesh.caretiger@gmail.com>
2019-10-27 16:23:18 -0500

    updates

--- pages/tachyons/lists.vue
+++ pages/tachyons/lists.vue
@@ -1,10 +1,28 @@
 <template>
     <div>
-        lists
+        <section>
+
+            <article class="debug">
+                <div>
+                     <img src="http://mrmrs.github.io/photos/cpu.jpg" class="db" alt="Photo of a dimly lit room with a computer interface terminal">
+                </div>
+                <div>
+                    <h1>Tech Giant Invests Huge Money</h1>
+                    <p>The tech giant says it is ready to begin planning a quantum computer, a powerful c</p>
+                    <p>By Robin Darnell</p>
+                </div>
+            </article>
+
+        </section>
 
     </div>
 </template>
 
 <style lang="scss" scoped>
+section {
 
+}
+h1 {
+    position: relative;
+}
 </style>
