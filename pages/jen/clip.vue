2e7da74df45e1b43c39f7564a4b07b51f80bad6c
venkateshvadlamani <venkatesh.caretiger@gmail.com>
2019-10-27 11:11:37 -0500

    updates

--- pages/jen/clip.vue
+++ pages/jen/clip.vue
@@ -1,42 +1,26 @@
 <template>
     <div class="pixel">
-        <h1 class="">Pixel the Kitten</h1>
+        <h1 class="">Complex polygon with border</h1>
         <h2>Like playing with grid</h2>
         <a href="https://bennettfeely.com/clippy/">Tool for editing clip path</a>             
         <figure>
-            <img src="/img/jen/people/19635836264_927fbee8b5_b.jpg" alt="">
-            <hr>
-            <img src="/img/jen/people/20070461390_e350b158e3_b.jpg" alt="">
-            <hr>
-            <img src="/img/jen/people/20258466845_9d192564f0_b.jpg" alt="">
+          <div class="shell">
+            <div class="poligon">
+              <img src="/img/jen/people/19635836264_927fbee8b5_b.jpg" alt="" >
+            </div>
+          </div>
         </figure>
-
-<div class="poligon">
-  <img src="http://lorempixel.com/g/600/400/">
-</div>
         
     </div>
 </template>
 
 <style lang="scss" scoped>
-.pixel {
-    
-}
-
-img {
-    width: 30vw;
-    height: 60vh;
-    object-fit: cover;
-    object-position: 50% 10%;
-    clip-path: polygon(12% 16%, 21% 12%, 32% 10%, 48% 10%, 48% 10%, 64% 12%, 67% 13%, 69% 14%, 74% 21%, 79% 24%, 83% 29%, 84% 29%, 86% 31%, 91% 37%, 94% 43%, 95% 52%, 95% 55%, 94% 65%, 92% 74%, 90% 78%, 87% 83%, 100% 96%, 90% 89%, 90% 89%, 91% 94%, 83% 97%, 79% 97%, 75% 100%, 65% 99%, 61% 99%, 55% 99%, 43% 99%, 36% 98%, 28% 91%, 28% 91%, 33% 88%, 23% 80%, 22% 83%, 12% 81%, 10% 79%, 10% 73%, 5% 62%, 4% 61%, 3% 54%, 3% 52%, 3% 47%, 3% 42%, 3% 40%, 5% 31%, 4% 29%, 6% 23%, 6% 22%, 9% 20%);
-    border: 10px red;
-}
 
 .poligon {
   display: inline-block;
   position: relative;
-  width: 250px;
-  height: 250px;
+  width: 40vmin;
+  height: 40vmin;
   background: lightgrey;
   box-sizing: border-box;
   -webkit-clip-path: polygon(12% 16%, 21% 12%, 32% 10%, 48% 10%, 48% 10%, 64% 12%, 67% 13%, 69% 14%, 74% 21%, 79% 24%, 83% 29%, 84% 29%, 86% 31%, 91% 37%, 94% 43%, 95% 52%, 95% 55%, 94% 65%, 92% 74%, 90% 78%, 87% 83%, 100% 96%, 90% 89%, 90% 89%, 91% 94%, 83% 97%, 79% 97%, 75% 100%, 65% 99%, 61% 99%, 55% 99%, 43% 99%, 36% 98%, 28% 91%, 28% 91%, 33% 88%, 23% 80%, 22% 83%, 12% 81%, 10% 79%, 10% 73%, 5% 62%, 4% 61%, 3% 54%, 3% 52%, 3% 47%, 3% 42%, 3% 40%, 5% 31%, 4% 29%, 6% 23%, 6% 22%, 9% 20%);
@@ -44,11 +28,13 @@ img {
 }
 
 .poligon img {
+  object-fit: cover;
+  object-position: 50% 10%;
   position: absolute;
   top: 20px; /* equal to border thickness */
   left: 20px; /* equal to border thickness */
-  width: 210px; /* container height - (border thickness * 2) */
-  height: 210px; /* container height - (border thickness * 2) */
+  width: calc(40vmin - 40px); /* container height - (border thickness * 2) */
+  height: calc(40vmin - 40px); /* container height - (border thickness * 2) */
   -webkit-clip-path: polygon(12% 16%, 21% 12%, 32% 10%, 48% 10%, 48% 10%, 64% 12%, 67% 13%, 69% 14%, 74% 21%, 79% 24%, 83% 29%, 84% 29%, 86% 31%, 91% 37%, 94% 43%, 95% 52%, 95% 55%, 94% 65%, 92% 74%, 90% 78%, 87% 83%, 100% 96%, 90% 89%, 90% 89%, 91% 94%, 83% 97%, 79% 97%, 75% 100%, 65% 99%, 61% 99%, 55% 99%, 43% 99%, 36% 98%, 28% 91%, 28% 91%, 33% 88%, 23% 80%, 22% 83%, 12% 81%, 10% 79%, 10% 73%, 5% 62%, 4% 61%, 3% 54%, 3% 52%, 3% 47%, 3% 42%, 3% 40%, 5% 31%, 4% 29%, 6% 23%, 6% 22%, 9% 20%);
   clip-path:  polygon(12% 16%, 21% 12%, 32% 10%, 48% 10%, 48% 10%, 64% 12%, 67% 13%, 69% 14%, 74% 21%, 79% 24%, 83% 29%, 84% 29%, 86% 31%, 91% 37%, 94% 43%, 95% 52%, 95% 55%, 94% 65%, 92% 74%, 90% 78%, 87% 83%, 100% 96%, 90% 89%, 90% 89%, 91% 94%, 83% 97%, 79% 97%, 75% 100%, 65% 99%, 61% 99%, 55% 99%, 43% 99%, 36% 98%, 28% 91%, 28% 91%, 33% 88%, 23% 80%, 22% 83%, 12% 81%, 10% 79%, 10% 73%, 5% 62%, 4% 61%, 3% 54%, 3% 52%, 3% 47%, 3% 42%, 3% 40%, 5% 31%, 4% 29%, 6% 23%, 6% 22%, 9% 20%);
 }
